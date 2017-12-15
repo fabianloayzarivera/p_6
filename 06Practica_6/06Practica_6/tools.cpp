a6e10e021bdc67e26d620074ccd9788736718ec0
Floayza <fabianloayzarivera@gmail.com>
2017-12-15 15:11:27 +0100

    fixed p6

---
+++ 06Practica_6/06Practica_6/tools.cpp
@@ -0,0 +1,33 @@
+#include "stdafx.h"
+#include "tools.h"
+
+int countWords(char *cad, const char* filename) {
+	int count = 0;
+	ifstream file("Read.txt");
+	char str[255];
+
+	while (file.getline(str, 255))
+	{
+		if (!strcmp(str, cad))
+			count++;
+	}
+
+	return count;
+}
+
+int sumNumbers(const char* filename) {
+	ifstream file("ReadNumbers.txt");
+	char str[255];
+	int sum = 0;
+	while (file.getline(str, 255))
+	{
+		stringstream ss(str);
+		int i;
+		while (ss >> i) {
+			sum += i;
+			if (ss.peek() == ',')
+				ss.ignore();
+		}
+	}
+	return sum;
+}
---
+++ 06Practica_6/06Practica_6/tools.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int countWords(char *cad, const char* filename);
+int sumNumbers(const char* filename);
